076cc572a3c5f303fc5517534132818b2922842b
Kamila Michel <kamilam1987@gmail.com>
2019-10-07 13:07:15 +0100

    Modules tutorial completed

---
+++ Modules/app.js
@@ -0,0 +1,6 @@
+//********************** MODULES AND REQUIRE *********************
+
+//Require function(path to the module)
+let counter = require('./count'); // Return and store in variable
+
+console.log(counter(['kate', 'orla', 'marry']));
