48749d899bee728783d6ecf9091e99aa51cb2258
Kamila Michel <kamilam1987@gmail.com>
2019-10-08 11:44:33 +0100

    Asynchronous method fixed

--- Reading-and-Writing-Files/app.js
+++ Reading-and-Writing-Files/app.js
@@ -2,15 +2,19 @@
 let fs = require('fs');
 //************** Synchronous method *******
 // Read a file
-let readMe = fs.readFileSync('readMe.txt', 'utf8');
-console.log(readMe);
+//let readMe = fs.readFileSync('readMe.txt', 'utf8');
+//console.log(readMe);
 
 // Write a file
-fs.writeFileSync('writeMe.txt', readMe);
+//fs.writeFileSync('writeMe.txt', readMe);
 
 //************** Asynchronous method *******
-//fs.readFile('readMe.txt', 'utf8', function(err, data){
-  //fs.writeFile('writeMe.txt', data);
-//});
+fs.readFile('readMe.txt', 'utf8', function(err, data){
+  console.log(data);
+  fs.writeFile("temp.txt", data, (err) => {
+    if (err) console.log(err);
+    console.log("Successfully Written to File.");
+  });
+});
 
   console.log('test');// Fire this first while is reading a file
